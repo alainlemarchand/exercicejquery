3f1bc82e0c8888947e28f629ead7bc47ff0c7212
Alain LEMARCHAND <alainlemarchand@macbook-pro-de-alain.home>
2020-11-06 14:28:16 +0100

    enregistrer exo Jquery

---
+++ Exercice 4/assets/js/jqueryexo4.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+$('#firstText').empty();
+});
+
+$(document).ready(function(){
+    $('#secondText').remove();
+    });
---
+++ exercice 1/assets/js/exo1.js
@@ -0,0 +1,45 @@
+/* Exo1
+$("text").hide();
+
+
+
+$(document).ready(function(){
+    $("#text").hide(); 
+})
+*/
+
+/* Exo2
+$(document).ready(function(){
+    $("#text").show();
+})
+
+
+bonus correction : 
+
+*/
+
+//exo3 
+
+$(document).ready(function(){
+    
+    $('#text').css('font-family','Arial');
+    
+    $('#text').css('border', '1px solid #000');
+
+})
+
+
+/*
+
+$(document).ready(function(){
+    $("#text").click(function(){
+        $(this).hide();
+    });
+})
+
+$(document).ready(function(){
+    $("#text").click(function(){
+        $(this).hide();
+    });
+})
+*/
